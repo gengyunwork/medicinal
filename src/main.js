0e11d14ce569314502aad07d9e5c76673d5bcd3c
DengSuning <deng_suning@163.com>
2019-08-06 13:38:54 +0800

    导入scss

--- src/main.js
+++ src/main.js
@@ -3,8 +3,37 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'normalize.css/normalize.css';
+
 import { Button } from "vant";
 Vue.use(Button);
+
+import { Cell, CellGroup } from 'vant';
+Vue.use(Cell).use(CellGroup);
+
+import { Icon } from 'vant';
+Vue.use(Icon);
+
+import { Image } from 'vant';
+Vue.use(Image);
+
+import { Row, Col } from 'vant';
+Vue.use(Row).use(Col);
+
+import { Popup } from 'vant';
+Vue.use(Popup);
+
+import { Toast } from 'vant';
+Vue.use(Toast);
+
+import { Grid, GridItem } from 'vant';
+Vue.use(Grid).use(GridItem);
+
+
+import { Lazyload } from 'vant';
+// options 为可选参数，无则不传
+Vue.use(Lazyload);
+
 
 
 Vue.config.productionTip = true;
@@ -14,3 +43,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
